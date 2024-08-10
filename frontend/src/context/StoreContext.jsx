63d27452a7a6211cc0e69bdd7390d64f8b2f9081
anuushrirao <anushkavshrirao@gmail.com>
2024-08-10 22:39:13 +0530

    fetch data to frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,12 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
-
+import axios from "axios";
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
     const url = "http://localhost:4000"
     const [token,setToken] = useState("")
-
+    const [food_list,setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if(!cartItems[itemId]) {
@@ -40,10 +39,19 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async() => {
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
     useEffect(()=>{
-        if(localStorage.getItem("token")){
-            setToken(localStorage.getItem("token"))
+        async function loadData(){
+            await fetchFoodList();
+            if(localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"))
+            }
         }
+        loadData();
     },[])
     
     {/*The useEffect hook in your code is used to perform a side effect 
