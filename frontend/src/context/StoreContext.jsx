1f49f3eb1ea974415f263e4a11931875a7a1499e
anuushrirao <anushkavshrirao@gmail.com>
2024-08-20 23:22:19 +0530

    changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,14 +8,18 @@ const StoreContextProvider = (props) => {
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if(!cartItems[itemId]) {
             setCartItems((prev)=>({...prev,[itemId]:1}))
         }
         else{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
+        if(token){
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
+
 
     {/*State Management: addToCart manages the cartItems 
         state, ensuring items are added or their quantities 
@@ -23,8 +27,11 @@ const StoreContextProvider = (props) => {
         Prop Passing: The parent component (App) passes the 
         addToCart function and cartItems state to the child component (Cart) as props. */}
     
-    const removeFromCarts = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    const removeFromCarts = async (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+        if(token){
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount = () =>{
@@ -44,11 +51,18 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async(token) => {
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(()=>{
         async function loadData(){
             await fetchFoodList();
             if(localStorage.getItem("token")){
                 setToken(localStorage.getItem("token"))
+                await loadCartData(localStorage.getItem("token"));
+                
             }
         }
         loadData();
